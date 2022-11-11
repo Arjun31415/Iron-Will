8cc48e4faffc98a496d83c12aca2cda4bfd27104
M-Manas-s <manastiwari111222@gmail.com>
2022-11-11 16:25:04 +0530

    added centers.php

--- centers.php
+++ centers.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="profile.css" />
+    <link rel="stylesheet" href="nav_bar.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iron Will - Trainers</title>
 </head>
